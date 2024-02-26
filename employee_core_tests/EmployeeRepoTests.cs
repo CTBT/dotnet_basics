6d840a87365d093a977ab1f0a8f3afb57b51e121
Christian Bertram <christian.bertram2@gmx.de>
2024-02-26 21:38:39 +0100

    Update EmployeeRepoTests.cs

--- employee_core_tests/EmployeeRepoTests.cs
+++ employee_core_tests/EmployeeRepoTests.cs
@@ -1,33 +1,29 @@
 using EmployeeCore.IO;
 using EmployeeCore.Repositories;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace employee_core_tests;
 
 public class EmployeeRepoTests
 {
-    [Fact]
-    public void GetEmployee_ReturnsEmployee()
+
+    private EmployeeService GetService()
     {
-        // Arrange
-        var reader = new EmployeeDataReader();
+        var reader = new EmployeeDataReader(NullLogger<EmployeeDataReader>.Instance);
         var repo = new EmployeeRepo(reader);
-
-        // Act
-        var result = repo.GetEmployee(1);
-
-        // Assert
-        Assert.True(result.Name == "Tester1");
-        result.Name.Should().Be("Tester1");
+        return new EmployeeService(NullLogger<EmployeeService>.Instance, repo);
     }
     
-// Make sure the following conditions are fulfilled:
-// 0. GetEmployee return null if the id was not found.
-// 1. GetEmployees returns the correct list of all employees when no filter is applied
-// 2. GetEmployees applies filters correctly
-    // Count
-    // OnlyContain
-// 3. GetSkillName returns an error if the skill is not found ( use https://fluentassertions.com/exceptions/)
-// 4. GetSkillName returns the name of the skill.
-// 5. GetSkills returns the correct list of skills
-// 6. GetSkills return a list of UNIQUE skills - make it green
+    [Fact]
+    public void Task1()
+    {
+        // Arrange
+        var service = GetService();
+        
+        // Act
+        var result = service.Task1();
+        
+        // Assert
+        Assert.True(false);
+    }
 }
