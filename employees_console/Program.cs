9aba6bf133933699f0d84b6f5600199391a41c34
Christian Bertram <christian.bertram2@gmx.de>
2024-02-26 20:55:55 +0100

    implementations

--- employees_console/Program.cs
+++ employees_console/Program.cs
@@ -25,23 +25,38 @@ var service = new EmployeeService(AppConfiguration.GetLogger<EmployeeService>(),
 
 try
 {
-    // 1. get employee with id=0 and print his name and location
-    service.Task1();
+    // 1. find someone in stuttgart that has all skills and has a doctor degree (starts with "Dr.") and print his name
+    var task1Result = service.Task1();
+    mainLogger.LogInformation("Found {EmployeeName}", task1Result?.Name);
 
     // 2. print the skill-names of that employee
-    service.Task2();
+    var task2Result = service.Task2(task1Result);
+    mainLogger.LogInformation("Employee skills:{Skills}",string.Join(",", task2Result));
 
     // 3. print the number of employees with the skill 'Database':
-    service.Task3();
+    var task3Result = service.Task3();
+    mainLogger.LogInformation("Employees with skill \'Database\': {Count}", task3Result);
 
     // 4. print 5 of the employees with the skill 'Database' and the location 'Bonn' ordered by name
-    service.Task4();
+    var task4Result = service.Task4();
+    mainLogger.LogInformation("5 employees with skill database in bonn:{Employees}",
+        string.Join(",",task4Result.Select(i => i.Name)));
 
     // 5. print the number of employees per skill
-    service.Task5();
+    var task5Results = service.Task5();
+    foreach (var skillCount in task5Results)
+    {
+        mainLogger.LogInformation("Skill: {SkillName} - {Count}", skillCount.Key.Name, skillCount.Value);
+    }
 
     // 6. Print the number of employees per location ordered by there number.
-    service.Task6();
+    var task6Result = service.Task6();
+    foreach (var pair in task6Result)
+    {
+        mainLogger.LogInformation("Location: {Location} - {Count} employees", 
+            pair.Key.ToString(), 
+            pair.Value.ToString());
+    }
 }
 catch (NotImplementedException)
 {
--- employees_core/Repositories/EmployeeService.cs
+++ employees_core/Repositories/EmployeeService.cs
@@ -6,45 +6,55 @@ namespace EmployeeCore.Repositories;
 public class EmployeeService(ILogger<EmployeeService> _logger, EmployeeRepo repo)
 {
     
-    public Employee Task1()
+    public Employee? Task1()
     {
         _logger.LogInformation("----- Result task 1 (employee-name): ------");
-
-        throw new NotImplementedException();
+        
+        return repo.GetEmployees()
+            .FirstOrDefault(i => i.Skills.Count( ) == 11 && i.Location == Location.Stuttgart && i.Name.StartsWith("Dr."));
     }
     
-    public string[] Task2()
+    public IEnumerable<string> Task2(Employee employee)
     {
         _logger.LogInformation("----- Result task 2 (skill-names): ------");
-
-        throw new NotImplementedException();
+        
+        return employee.Skills
+            .Join(repo.GetSkills(), i => i, skill => skill.Id, (i, skill) => skill.Name);
     }
     
     public int Task3()
     {
         _logger.LogInformation("----- Result task 3 (employee-counts): ------");
         
-        throw new NotImplementedException();
+        return repo
+            .GetEmployees("Database")
+            .Count();
     }
     
-    public Employee[] Task4()
+    public IOrderedEnumerable<Employee> Task4()
     {
         _logger.LogInformation("----- Result task 4 (database experts in bonn): ------");
-        
-        throw new NotImplementedException();
+        return repo
+            .GetEmployees("Database", Location.Bonn)
+            .Take(5)
+            .OrderBy(i => i.Name);
     }
     
     public Dictionary<Skill, int> Task5()
     {
         _logger.LogInformation("----- Result task 5 (employees per skill): ------");
-        
-        throw new NotImplementedException();
+
+        return repo.GetSkills()
+            .ToDictionary(skill => skill, skill => repo.GetEmployees(skill.Name).Count());
     }
     
-    public Dictionary<Skill, int> Task6()
+    public Dictionary<Location, int> Task6()
     {
         _logger.LogInformation("----- Result task 6 (employees per location ordered): ------");
-        
-        throw new NotImplementedException();
+
+        return repo.GetEmployees()
+            .GroupBy(i => i.Location)
+            .ToDictionary(t=> t.Key, t=> t.Count())
+            .OrderBy(i => i.Value).ToDictionary();
     }
 }
