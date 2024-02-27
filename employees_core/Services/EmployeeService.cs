7822f4e205c6aee333e16b2cac5fbb0f01169643
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2024-02-27 17:26:58 +0100

    Update EmployeeService.cs

--- employees_core/Services/EmployeeService.cs
+++ employees_core/Services/EmployeeService.cs
@@ -11,7 +11,7 @@ public class EmployeeService(ILogger<EmployeeService> _logger, EmployeeFileReade
     {
         _logger.LogInformation("----- Result task 1 (employee-name): ------");
         
-        return repo.GetEmployees()
+        return repo.GetEmployeeData()
             .FirstOrDefault(i => i.Skills.Count( ) == 11 && i.Location == Location.Stuttgart && i.Name.StartsWith("Dr."));
     }
     
@@ -20,7 +20,7 @@ public class EmployeeService(ILogger<EmployeeService> _logger, EmployeeFileReade
         _logger.LogInformation("----- Result task 2 (skill-names): ------");
         
         return employee.Skills
-            .Join(repo.GetSkills(), i => i, skill => skill.Id, (i, skill) => skill.Name);
+            .Join(repo.GetSkillData(), i => i, skill => skill.Id, (i, skill) => skill.Name);
     }
     
     public int Task3()
@@ -28,15 +28,16 @@ public class EmployeeService(ILogger<EmployeeService> _logger, EmployeeFileReade
         _logger.LogInformation("----- Result task 3 (employee-counts): ------");
         
         return repo
-            .GetEmployees("Database")
-            .Count();
+            .GetEmployeeData()
+            .Count(i => i.Skills.Contains(5));
     }
     
     public IOrderedEnumerable<Employee> Task4()
     {
         _logger.LogInformation("----- Result task 4 (database experts in bonn): ------");
         return repo
-            .GetEmployees("Database", Location.Bonn)
+            .GetEmployeeData()
+            .Where(i => i.Skills.Contains(5) && i.Location == Location.Bonn)
             .Take(5)
             .OrderBy(i => i.Name);
     }
@@ -45,15 +46,16 @@ public class EmployeeService(ILogger<EmployeeService> _logger, EmployeeFileReade
     {
         _logger.LogInformation("----- Result task 5 (employees per skill): ------");
 
-        return repo.GetSkills()
-            .ToDictionary(skill => skill, skill => repo.GetEmployees(skill.Name).Count());
+        return repo.GetSkillData()
+            .ToDictionary(skill => skill, skill => repo.GetEmployeeData()
+                .Count(i => i.Skills.Contains(skill.Id)));
     }
     
     public Dictionary<Location, int> Task6()
     {
         _logger.LogInformation("----- Result task 6 (employees per location ordered): ------");
 
-        return repo.GetEmployees()
+        return repo.GetEmployeeData()
             .GroupBy(i => i.Location)
             .ToDictionary(t=> t.Key, t=> t.Count())
             .OrderBy(i => i.Value).ToDictionary();
