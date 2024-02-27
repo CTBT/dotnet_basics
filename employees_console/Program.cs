e9e0537c983e2cc6757f9fba26189c9210f44e1a
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2024-02-27 17:32:23 +0100

    introduce variables

--- employees_console/Program.cs
+++ employees_console/Program.cs
@@ -1,5 +1,6 @@
 using EmployeeConsole.Utilities;
 using EmployeeCore.IO;
+using EmployeeCore.Models;
 using EmployeeCore.Services;
 using Microsoft.Extensions.Logging;
 
@@ -25,22 +26,22 @@ var service = new EmployeeService(AppConfiguration.GetLogger<EmployeeService>(),
 try
 {
     // 1. find someone in stuttgart that has all skills and has a doctor degree (starts with "Dr.") and print his name
-    var task1Result = service.Task1();
+    var task1Result = service.Task1_GetEmployee();
 
     // 2. print the skill-names of that employee
-    var task2Result = service.Task2(task1Result!);
+    var task2Result = service.Task2_GetSkillNames(task1Result!);
 
     // 3. print the number of employees with the skill 'Database':
-    var task3Result = service.Task3();
+    var task3Result = service.Task3_GetEmployeeCounts(5);
 
     // 4. print 5 of the employees with the skill 'Database' and the location 'Bonn' ordered by name
-    var task4Result = service.Task4();
+    var task4Result = service.Task4_GetExperts(5, Location.Bonn);
 
     // 5. print the number of employees per skill
-    var task5Results = service.Task5();
+    var task5Results = service.Task5_GetEmployeesPerSkill();
 
     // 6. Print the number of employees per location ordered by there number.
-    var task6Result = service.Task6();
+    var task6Result = service.Task6_GetEmployeesPerLocation();
 }
 catch (NotImplementedException)
 {
--- employees_core/Services/EmployeeService.cs
+++ employees_core/Services/EmployeeService.cs
@@ -7,37 +7,37 @@ namespace EmployeeCore.Services;
 public class EmployeeService(ILogger<EmployeeService> _logger, EmployeeFileReader repo)
 {
     
-    public Employee? Task1()
+    public Employee? Task1_GetEmployee()
     {
         _logger.LogInformation("----- Result task 1 (employee-name): ------");
         throw new NotImplementedException();
     }
     
-    public IEnumerable<string> Task2(Employee employee)
+    public IEnumerable<string> Task2_GetSkillNames(Employee employee)
     {
         _logger.LogInformation("----- Result task 2 (skill-names): ------");
         throw new NotImplementedException();
     }
     
-    public int Task3()
+    public int Task3_GetEmployeeCounts(int skillId)
     {
         _logger.LogInformation("----- Result task 3 (employee-counts): ------");
         throw new NotImplementedException();
     }
     
-    public IOrderedEnumerable<Employee> Task4()
+    public IOrderedEnumerable<Employee> Task4_GetExperts(int skillId, Location location)
     {
         _logger.LogInformation("----- Result task 4 (database experts in bonn): ------");
         throw new NotImplementedException();
     }
     
-    public Dictionary<Skill, int> Task5()
+    public Dictionary<Skill, int> Task5_GetEmployeesPerSkill()
     {
         _logger.LogInformation("----- Result task 5 (employees per skill): ------");
         throw new NotImplementedException();
     }
     
-    public Dictionary<Location, int> Task6()
+    public Dictionary<Location, int> Task6_GetEmployeesPerLocation()
     {
         _logger.LogInformation("----- Result task 6 (employees per location ordered): ------");
         throw new NotImplementedException();
