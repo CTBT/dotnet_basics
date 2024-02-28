07e81d6d693d815c784fa819feec9e3e67de819f
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2024-02-28 22:46:23 +0100

    Update Program.cs

--- employees_api/Program.cs
+++ employees_api/Program.cs
@@ -1,6 +1,8 @@
+using System.Text.Json.Serialization;
 using EmployeeCore.IO;
 using EmployeeCore.Models;
 using EmployeeCore.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,16 @@ builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeFileReader>();
 builder.Services.AddOutputCache();
     
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,20 +37,51 @@ if (app.Environment.IsDevelopment())
 app.UseOutputCache();
 app.UseHttpsRedirection();
 
-app.MapGet("/employee", FindEmployeeByIdRequestHandler)
+app.MapGet("/employee/{id}", FindEmployeeByIdRequestHandler)
 .WithName("FindEmployeeById")
+.Produces<Employee>()
 .WithOpenApi()
 .CacheOutput();
+
+
+app.MapGet("/employees", GetEmployees)
+    .WithName("GetEmployees")
+    .Produces<Employee>()
+    .WithOpenApi()
+    .CacheOutput();
+
 
 app.Run();
 
 
 // improve your api:
 // add output caching (https://learn.microsoft.com/en-us/aspnet/core/performance/caching/output?view=aspnetcore-8.0)
+// work with http status codes
 // add error handling
 
-Employee? FindEmployeeByIdRequestHandler([FromServices]EmployeeService service, [FromQuery]int  id)
+Results<NotFound, BadRequest, Ok<Employee>> FindEmployeeByIdRequestHandler([FromServices]EmployeeService service, [FromRoute]int  id)
 {
+    if (id < 0)
+    {
+        return TypedResults.BadRequest();
+    }
+    
     var employee = service.FindEmployee(id);
-    return employee;
+
+    return employee is not null ? TypedResults.Ok(employee) : TypedResults.NotFound();
+}
+
+
+Results<BadRequest<string>, Ok<IOrderedEnumerable<Employee>>> GetEmployees([FromServices]EmployeeService service, [FromQuery] int skillId, [FromQuery]Location location)
+{
+    if (service.GetSkills().All(i => i.Id != skillId))
+    {
+        return TypedResults.BadRequest("Skill filter is invalid");
+    }
+    
+    var result = service.GetEmployees(skillId, location)
+        .Take(100)
+        .OrderBy(i => i.Name);
+
+    return TypedResults.Ok(result);
 }
