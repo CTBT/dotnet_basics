afb84955ad7f9dae54fff8cdbd94e495677b7a7a
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2024-02-28 22:06:17 +0100

    Update Program.cs

--- employees_api/Program.cs
+++ employees_api/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeFileReader>();
 builder.Services.AddOutputCache();
-
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,7 +34,7 @@ app.Run();
 
 
 // improve your api:
-// add output caching
+// add output caching (https://learn.microsoft.com/en-us/aspnet/core/performance/caching/output?view=aspnetcore-8.0)
 // add error handling
 
 Employee? FindEmployeeByIdRequestHandler([FromServices]EmployeeService service, [FromQuery]int  id)
