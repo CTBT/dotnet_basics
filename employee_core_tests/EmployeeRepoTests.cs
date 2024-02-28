617b39822b674b3a7f8d6b4b4e6cfc8b974c65ed
Christian Bertram <Christian.Bertram.Extern@deutschepost.de>
2024-02-28 15:14:29 +0100

    add mock

--- employee_core_tests/EmployeeRepoTests.cs
+++ employee_core_tests/EmployeeRepoTests.cs
@@ -1,16 +1,24 @@
 using EmployeeCore.IO;
+using EmployeeCore.Models;
 using EmployeeCore.Services;
+using FluentAssertions;
 using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
 
 namespace employee_core_tests;
 
 public class EmployeeRepoTests
 {
 
-    private EmployeeService GetService()
+    private EmployeeService SetupTest(IEnumerable<Employee> employeeTestData, IEnumerable<Skill> skillTestData)
     {
-        var reader = new EmployeeFileReader(NullLogger<EmployeeFileReader>.Instance);
-        return new EmployeeService(NullLogger<EmployeeService>.Instance, reader);
+        var readerMoq = new Mock<IEmployeeRepository>();
+        readerMoq.Setup(reader => reader.GetEmployeeData())
+            .Returns(employeeTestData);
+        readerMoq.Setup(reader => reader.GetSkillData())
+            .Returns(skillTestData);
+        
+        return new EmployeeService(NullLogger<EmployeeService>.Instance, readerMoq.Object);
     }
     
     // write tests for the methods in the EmployeeService class
@@ -24,12 +32,19 @@ public class EmployeeRepoTests
     public void Task1()
     {
         // Arrange
-        var service = GetService();
+        var testSkills = new List<Skill>()
+        {
+            new() { Id = 1, Name = "TestSkill1" },
+            new() { Id = 2, Name = "TestSkill2" }
+        };
+        var testEmployees = new List<Employee>();
+        
+        var service = SetupTest(testEmployees, testSkills);
         
         // Act
         var result = service.Task1_GetEmployee();
         
         // Assert
-        Assert.True(false);
+        result.Should().BeNull();
     }
 }
